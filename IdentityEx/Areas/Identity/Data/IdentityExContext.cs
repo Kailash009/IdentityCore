ec840f7cba0b88350aef687d7b185937f44303ba
kailash <programmer.joshi@gmail.com>
2023-07-15 16:31:09 +0530

    CrudCore

---
+++ IdentityEx/Areas/Identity/Data/IdentityExContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentityEx.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityEx.Data;
+
+public class IdentityExContext : IdentityDbContext<IdentityExUser>
+{
+    public IdentityExContext(DbContextOptions<IdentityExContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ IdentityEx/Migrations/20230715100030_initialtest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityEx.Migrations
+{
+    public partial class initialtest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
